ee622112a49857aad12f030db322d7b73ae6dd7b
Anandu R <ananduraj32@gmail.com>
2021-04-16 16:04:06 +0530

    further developments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FacilitiesComponent } from './facilities/facilities.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FacilitiesComponent
   ],
   imports: [
     BrowserModule,
