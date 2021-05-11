4d0ee3e440454c54a33f48dbd0f868a29e23a552
aishmike <aishmike99@gmail.com>
2021-05-11 21:15:00 +0530

    images

---
+++ src/private_config.ts
@@ -0,0 +1,10 @@
+export const PrivateConfig = {
+    firebase : {
+        apiKey : 'AIzaSyAqvcf9AHA8dFSWRWd2nLSf449i2q8fVB0',
+        authDomain: 'odu-komban-ok.firebaseapp.com',
+        databaseURL: 'https://odu-komban-ok.firebaseio.com',
+        projectId: 'odu-komban-ok',
+        storageBucket: 'odu-komban-ok.appspot.com',
+        messagingSenderId: "209002783516"
+    }
+}
