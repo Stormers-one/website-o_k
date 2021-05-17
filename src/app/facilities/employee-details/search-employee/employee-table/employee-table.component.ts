abd51f32641c3d3b9bbbf5d6edc996523a0ceae3
Anandu R <ananduraj32@gmail.com>
2021-05-17 17:52:21 +0530

    Update employee-table.component.ts

--- src/app/facilities/employee-details/search-employee/employee-table/employee-table.component.ts
+++ src/app/facilities/employee-details/search-employee/employee-table/employee-table.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import Employee from 'src/app/shared/models/employee';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
+import { EmployeeService } from 'src/app/shared/services/employee.service';
 
 @Component({
   selector: 'app-employee-table',
@@ -12,7 +13,7 @@ export class EmployeeTableComponent implements OnInit {
 
   @Input() employee: Employee[] = [];
 
-  constructor(private router: Router, private _location: Location) { 
+  constructor(private router: Router, private _location: Location, private employeeService: EmployeeService) { 
     console.log(this.employee);
     }
 
@@ -27,4 +28,7 @@ export class EmployeeTableComponent implements OnInit {
     this._location.back();
   }
 
+  delete(employee: Employee) {
+    this.employeeService.removeEmployee(employee);
+  }
 }
