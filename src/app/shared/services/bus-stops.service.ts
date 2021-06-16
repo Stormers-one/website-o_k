4a1e5c42cbe0e2cad50705ce90512b3d63a16120
Anandu R <ananduraj32@gmail.com>
2021-06-16 23:29:57 +0530

    stop service

--- src/app/shared/services/bus-stops.service.ts
+++ src/app/shared/services/bus-stops.service.ts
@@ -16,9 +16,31 @@ export class BusStopsService {
   getStops() {
     return this.stops;
   }
-
+  res_id:any = []
+  res:any = []
   async getRoutes(route:string){
-    const res = this.db.collection('Routes', ref => ref.where('route_name','==',route))
-    console.log(res)
+    const filteredData = this.db.collection('Routes', ref => ref.where('route_name','==',route))
+    this.res_id = await new Promise(resolve => {
+      filteredData.snapshotChanges().subscribe((data) => {
+        let dat = resolve(data.map((a) => {
+          let id = a.payload.doc.id;
+          return id;
+        })); return dat;
+      })
+    });
+    let res
+    for(var _id in this.res_id){
+      res = await new Promise(resolve => { 
+        this.routes.doc(_id).collection('stops').snapshotChanges().subscribe((data)=>{
+          let stops = resolve(data.map((e)=>{
+            const data = e.payload.doc.data()
+            return {...data}
+          })); 
+          return stops
+        });
+      });
+      this.res.push(res)
+    }
+    console.log(this.res)
   }
 }
